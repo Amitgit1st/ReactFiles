0d93aa6c86560f17ef95ed1d0c6355ddd9bef6cb
Amitgit1st <aamitp6297@gmail.com>
2023-08-06 19:06:02 +0530

    ExpenseItem

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div><h2>Expense Items</h2></div>
+            <div>
+                <h2>Food</h2>
+                <h2>Rs10</h2>
+            </div>
+            <div>
+                <h2>Petrol</h2>
+                <h2>Rs100</h2>
+            </div>
+            <div>
+                <h2>Movie</h2>
+                <h2>Rs200</h2>
+            </div>
+        </div>
+    )
+}
+export default ExpenseItem;
