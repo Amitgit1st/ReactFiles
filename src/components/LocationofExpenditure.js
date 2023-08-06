8cb1a6e3224ecf1522c79c833710fd152077e44e
Amitgit1st <aamitp6297@gmail.com>
2023-08-07 00:23:02 +0530

    LocationOfExpenditure(props)

--- src/components/LocationofExpenditure.js
+++ src/components/LocationofExpenditure.js
@@ -1,20 +1,18 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-    const expenseDate = new Date(2023, 7, 6);
-    const expenseTitle = "Car Insurance";
-    const expenseAmount = 4000;
-    const LocationOfExpenditure = "Maharashtra"
+function LocationOfExpenditure(props) {
+   
 
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toISOString()}</div>
             <div className='expense-item__description'></div>
-            <h2>{LocationOfExpenditure}</h2>
+            <h2>{props.date.toISOString()}</h2>
             <div className='expense-item__description'></div>
-            <h2>{expenseTitle}</h2>
-            <div className='expense-item__price'>Rs{expenseAmount}</div>
+            <h2>{props.location}</h2>
+            <div className='expense-item__description'></div>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>Rs{props.amount}</div>
         </div>
     )
 }
-export default ExpenseItem;
+export default LocationOfExpenditure;
