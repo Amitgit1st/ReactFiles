49299df59733e102fbe5d75969a95b24dc879b4c
Amitgit1st <aamitp6297@gmail.com>
2023-08-07 12:12:36 +0530

    DateIsReadable

--- src/components/LocationofExpenditure.js
+++ src/components/LocationofExpenditure.js
@@ -1,12 +1,22 @@
 import './ExpenseItem.css'
 
 function LocationOfExpenditure(props) {
+
+    // In the previous code DATE is not readable
+    // here in this code MONTH DAY & YEAR are readable 
+    const month=props.date.toLocaleString('en-US',{month:'long'});
+    const day=props.date.toLocaleString('en-US',{day:'2-digit'});
+    const year=props.date.getFullYear();
    
 
     return (
         <div className='expense-item'>
-            <div className='expense-item__description'></div>
-            <h2>{props.date.toISOString()}</h2>
+            <div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+            </div>
+            
             <div className='expense-item__description'></div>
             <h2>{props.location}</h2>
             <div className='expense-item__description'></div>
