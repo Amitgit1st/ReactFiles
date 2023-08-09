3a038d33496e0f489d95478640a4b6747e57bd07
Amitgit1st <aamitp6297@gmail.com>
2023-08-09 21:51:06 +0530

    updateAmount

--- src/components/Expenses/LocationofExpenditure.js
+++ src/components/Expenses/LocationofExpenditure.js
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import './LocationofExpenditure.css'
 
 
 const LocationOfExpenditure=(props)=> {
-    const Delete=()=>{
-        console.log('clicked');
+    const [amount,setAmount]=useState(props.amount)
+    
+    const updateAmount=()=>{
+        setAmount('100$')
+       
     }
 
 return (
@@ -13,8 +17,8 @@ return (
             <ExpenseDate date={props.date}></ExpenseDate>
             <div className='expense-item__description'></div>
             <h2>{props.location}</h2>
-            <ExpenseDetails  title={props.title} amount={props.amount} ></ExpenseDetails>
-            <button onClick={Delete}>DeleteExpense</button>
+            <ExpenseDetails  title={props.title} amount={amount} ></ExpenseDetails>
+            <button onClick={updateAmount}>updateExpense</button>
            
         </div>
     );
